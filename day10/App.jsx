a9a7185d670fe1c76a76e9c11b5059466f38ba35
Raghavvram <raghavvram.johnson@gmail.com>
2025-03-17 17:31:47 +0530

    Add files via upload

---
+++ day10/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import EmployeeCard from './EmployeeCard';
+import Header from './Header';
+import EmployeeDashboard from './EmployeeDashboard';
+
+function App() {
+    return (
+        <div>
+            <EmployeeDashboard />
+        </div>
+    )
+}
+
+export default App
